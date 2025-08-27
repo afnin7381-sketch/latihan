fe066f84544d96529915e4a296c4a38cb0c71026
afnin7381-sketch <afnin7381@gmail.com>
2025-08-27 13:50:42 +0700

    Add files via upload

---
+++ latihan02RGB.ino
@@ -0,0 +1,39 @@
+void setup() {
+pinMode (2, OUTPUT);
+pinMode (3, OUTPUT); 
+pinMode (4, OUTPUT);
+}
+
+void loop() {
+digitalWrite (2, LOW);// merah
+digitalWrite (3, HIGH);
+digitalWrite (4, HIGH);
+delay (1000);
+
+digitalWrite (2, HIGH);// biru
+digitalWrite (3, LOW);
+digitalWrite (4, HIGH);
+delay (1000);
+
+digitalWrite (2, HIGH); // hijau
+digitalWrite (3, HIGH);
+digitalWrite (4, LOW);
+delay (1000);
+
+digitalWrite (2, LOW);// ungu
+digitalWrite (3, LOW);
+digitalWrite (4, HIGH);
+delay (1000);
+
+digitalWrite (2, LOW);// kuning
+digitalWrite (3, HIGH);
+digitalWrite (4, LOW);
+delay (1000);
+
+digitalWrite (2, HIGH);// cyan
+digitalWrite (3, LOW);
+digitalWrite (4, LOW);
+delay (1000);
+}
+
+
