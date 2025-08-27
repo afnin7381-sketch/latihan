284728f61eb22937637f5df4e714691da63a4ca2
afnin7381-sketch <afnin7381@gmail.com>
2025-08-27 13:50:15 +0700

    Add files via upload

---
+++ buzzerlatihan02.ino
@@ -0,0 +1,12 @@
+void setup() {
+pinMode (2, OUTPUT);// put your setup code here, to run once:
+
+}
+
+void loop() {
+digitalWrite (2, LOW);// put your main code here, to run repeatedly:
+delay (2000);
+digitalWrite (2, HIGH);
+delay (2000);
+
+}
